a728ef90da5de457af0bb97c6af391ece3e1899e
cyclowns <cyclowns@protonmail.ch>
2018-05-24 20:48:07 -0700

    It Just Works

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 const SteamID = require('steamid');
+const Promise = require('promise');
+const readline = require('readline');
 
 const apikey = "E84E479671BF539828C2D471A9FF835C"; //who cares
 
@@ -8,15 +10,15 @@ var helpers = //object of helper functions for steam/logs api
 {
 	getSteamId: function(username) //returns steamid64 from vanity url
 	{
-		return fetch(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${apikey}&vanityurl=${username}&format=json`)
+		return fetch(`https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${apikey}&vanityurl=${username}`)
 			.then(res => res.json())
 			.then(json => json.response.steamid)
 			.catch(error => console.error(error));
 	},
 
-	getLogsProfile: function(vanityurl) //returns logs.tf profile url from vanity url
+	getLogsProfile: function(id) //returns logs.tf profile url from steam id
 	{
-		return `https://logs.tf/profile/${vanityurl}`;
+		return `https://logs.tf/profile/${id}`;
 	},
 
 	fetchPlayerLogs: function(id) //returns all logs from a player id
@@ -39,7 +41,7 @@ var helpers = //object of helper functions for steam/logs api
 	getClassStatsFromLog: async function(log, id)
 	{
 		var sid = new SteamID(id);
-		var steam3ID = sid.getSteam3RenderedID(); //ugh
+		var steam3ID = sid.getSteam3RenderedID(); //ugh, id conversion
 
 		var data = await this.getLogDataFromID(log);
 
@@ -48,10 +50,8 @@ var helpers = //object of helper functions for steam/logs api
 		Object.keys(players).forEach(function(key) 
 		{
 			var player = players[key];
-			console.log(`found player ${key}`);
 			if(key == steam3ID)
 			{
-				console.log("found");
 				stats = player;
 			}
 		});
@@ -63,39 +63,78 @@ var helpers = //object of helper functions for steam/logs api
 		var logs = await this.fetchPlayerLogs(id);
 		var sliced_logs = this.sliceLogList(logs, num);
 		
-		var dpm_sum = 0;
-		var kills_sum = 0;
-		var deaths_sum = 0;
-		var assists_sum = 0;
-
-		sliced_logs.forEach(async function(item){
-			var log_id = item.id;
-
-			var stats = await helpers.getClassStatsFromLog(log_id, id);
-			if(stats)
-			{
-				console.log("yes");
-				dpm_sum += stats["dapm"];
-				kills_sum += stats["kills"];
-				deaths_sum += stats["deaths"];
-				assists_sum += stats["assists"];
-				console.log(stats["dapm"]);
+		var stat_list = {
+			dpm: {
+				sum: 0,
+				avg: 0
+			},
+			kills: {
+				sum: 0,
+				avg: 0
+			},
+			assists: {
+				sum: 0,
+				avg: 0
+			},
+			deaths: {
+				sum: 0,
+				avg: 0
 			}
+		};
+
+		//I FUCKIN HATE PROMISES AAAAAAAAAA
+		var counter = sliced_logs.length - 1;
+		var log_promise = new Promise(function(resolve, reject){
+			sliced_logs.forEach(function(item)
+			{
+				var log_id = item.id;
+
+				var stats = helpers.getClassStatsFromLog(log_id, id)
+					.then(function(stats)
+					{
+						//ugly but whatever
+						stat_list.dpm.sum  += stats["dapm"];
+						stat_list.kills.sum += stats["kills"];
+						stat_list.deaths.sum += stats["deaths"];
+						stat_list.assists.sum += stats["assists"];
+						if(counter === 0)
+						{
+							resolve();
+						}
+						counter--;
+					});
+			});
 		});
 
-		var dpm_avg = dpm_sum / num;
-		var kills_avg = kills_sum / num;
-		var deaths_avg = deaths_sum / num;
-		var assists_avg = assists_sum / num;
+		return log_promise.then(function(){
+			stat_list.dpm.avg = stat_list.dpm.sum / num;
+			stat_list.kills.avg = stat_list.kills.sum / num;
+			stat_list.deaths.avg = stat_list.deaths.sum / num;
+			stat_list.assists.avg = stat_list.assists.sum / num;
+			return stat_list;
+		});
 
-		console.log(dpm_avg + " is dpm");
 	}
 }
 
-//helpers.fetchPlayerLogs(`76561198062263639`)
-//	.then(logs => console.log(helpers.sliceLogList(logs, 3)))
+function startInput() //starts the input in console
+{
+	process.argv.forEach(async function(val, index, array){
+		if(index == 0 || index == 1) return;
+		console.log(`Stats for player ${index - 1}, ${val}`);
+		var id = await helpers.getSteamId(val);
+		var stats = await helpers.getAvgPlayerStats(id, 15)
+		console.log(`for the past 15 logs:`)
+		console.log(`Average DPM: ${stats.dpm.avg}`);
+		console.log(`Average kills: ${stats.kills.avg}`);
+		console.log(`Average deaths: ${stats.deaths.avg}`);
+		console.log(`Average assists: ${stats.assists.avg}`);
+	});
+}
 
-//helpers.getLogDataFromID(`2030456`)
-//	.then(res => console.log(res));
+startInput();
 
-helpers.getAvgPlayerStats(`76561198062263639`, 3);
+//helpers.getAvgPlayerStats(`76561198062263639`, 5)
+//	.then(function(statlist){
+//		console.log(statlist.kills.avg);
+//	});
